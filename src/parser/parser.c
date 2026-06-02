0430c709d007ac7dad1a518285039cc00f56dbae
cheng8214 <cheng8214@163.com>
2026-06-02 17:25:50 +0800

    修复 Font 和 Image 类型关键字在 parser 中未识别的问题

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -82,6 +82,8 @@ Ast* parse_statement(Parser* p) {
         case TOK_DRAW_TYPE:
         case TOK_EVENT_TYPE:
         case TOK_RGB_TYPE:
+        case TOK_IMAGE_TYPE:
+        case TOK_FONT_TYPE:
         case TOK_STYLE_TYPE:
         case TOK_PTR_TYPE:
             stmt = parse_var_decl_internal(p);
