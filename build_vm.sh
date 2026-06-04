aab8654e80b96e2cab0e142b402f02de08c54aa0
cheng8214 <cheng8214@163.com>
2026-06-04 08:23:25 +0800

    fix: 补充遗漏的image_load_ex_test注释修复，添加Linux构建脚本

---
+++ build_vm.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+set -e
+
+echo "Building LenoLang VM Runtime (no compiler)..."
+
+mkdir -p build
+
+SOURCES=""
+SOURCES="$SOURCES src/vm_main.c"
+SOURCES="$SOURCES src/error.c"
+SOURCES="$SOURCES src/scope.c"
+SOURCES="$SOURCES src/gc.c"
+SOURCES="$SOURCES src/value.c"
+SOURCES="$SOURCES src/string_table.c"
+SOURCES="$SOURCES src/object/object_string.c"
+SOURCES="$SOURCES src/object/object_array.c"
+SOURCES="$SOURCES src/object/object_dict.c"
+SOURCES="$SOURCES src/object/object_number.c"
+SOURCES="$SOURCES src/object/object_file.c"
+SOURCES="$SOURCES src/object/object_struct.c"
+SOURCES="$SOURCES src/object/object_face.c"
+SOURCES="$SOURCES src/object/object_cstruct.c"
+SOURCES="$SOURCES src/object/object_thread.c"
+SOURCES="$SOURCES src/object/object_event.c"
+SOURCES="$SOURCES src/object/object_draw.c"
+SOURCES="$SOURCES src/object/object_window.c"
+SOURCES="$SOURCES src/object/object_font.c"
+SOURCES="$SOURCES src/object/object_image.c"
+SOURCES="$SOURCES src/bound_method.c"
+SOURCES="$SOURCES src/coroutine.c"
+SOURCES="$SOURCES src/vm/vm.c"
+SOURCES="$SOURCES src/debug.c"
+SOURCES="$SOURCES src/type.c"
+SOURCES="$SOURCES src/native.c"
+SOURCES="$SOURCES src/bigint.c"
+SOURCES="$SOURCES src/module_loader.c"
+SOURCES="$SOURCES src/module_dispatch.c"
+SOURCES="$SOURCES src/module_symbol_table.c"
+SOURCES="$SOURCES src/module.c"
+SOURCES="$SOURCES src/module/io/io.c"
+SOURCES="$SOURCES src/module/types/types.c"
+SOURCES="$SOURCES src/module/times/times.c"
+SOURCES="$SOURCES src/module/arrays/arrays.c"
+SOURCES="$SOURCES src/module/strings/strings.c"
+SOURCES="$SOURCES src/module/maths/maths.c"
+SOURCES="$SOURCES src/module/dicts/dicts.c"
+SOURCES="$SOURCES src/module/structs/structs.c"
+SOURCES="$SOURCES src/module/cstructs/cstructs.c"
+SOURCES="$SOURCES src/module/rands/rands.c"
+SOURCES="$SOURCES src/module/files/files.c"
+SOURCES="$SOURCES src/module/asyncs/asyncs.c"
+SOURCES="$SOURCES src/module/dirs/dirs.c"
+SOURCES="$SOURCES src/module/jsons/jsons.c"
+SOURCES="$SOURCES src/module/sockets/sockets.c"
+SOURCES="$SOURCES src/module/ffi/ffi.c"
+SOURCES="$SOURCES src/module/threads/threads.c"
+SOURCES="$SOURCES src/module/assert/assert.c"
+SOURCES="$SOURCES src/module/sys/sys.c"
+SOURCES="$SOURCES src/module/regexs/regexs.c"
+SOURCES="$SOURCES src/module/guis/guis.c"
+SOURCES="$SOURCES src/module/guis/guis_constants.c"
+SOURCES="$SOURCES src/module/guis/guis_draw.c"
+SOURCES="$SOURCES src/module/guis/guis_window.c"
+SOURCES="$SOURCES src/module/guis/guis_event.c"
+SOURCES="$SOURCES src/module/guis/guis_font.c"
+SOURCES="$SOURCES src/module/guis/guis_image.c"
+SOURCES="$SOURCES src/module/guis/leno_guis_log.c"
+SOURCES="$SOURCES src/serialize/serialize.c"
+
+# Platform-specific sources
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    SOURCES="$SOURCES src/module/guis/leno_guis_macos.c"
+    SOURCES="$SOURCES src/platform/platform_thread.c"
+elif [[ "$OSTYPE" == "linux"* ]]; then
+    SOURCES="$SOURCES src/module/guis/leno_guis_linux.c"
+    SOURCES="$SOURCES src/platform/platform_thread.c"
+fi
+
+# Platform-specific libraries
+LIBS="-lm -lpthread -ldl"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    LIBS="$LIBS -framework AppKit"
+else
+    LIBS="$LIBS -lX11"
+fi
+
+gcc -o build/leno_vm $SOURCES -Isrc -Wall -Wextra -std=c99 -O2 -s $LIBS -DLENO_VM_ONLY
+
+echo "VM build successful"
+echo ""
+echo "Usage: build/leno_vm <file.lenb>"
+echo "Self:  build/leno_vm --self"
---
+++ leno_lsp/build_lsp.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+set -e
+
+echo "Building Leno LSP Server with LenoC compiler..."
+
+mkdir -p build
+
+# Delete old executable if exists
+if [ -f build/leno_lsp ]; then
+    rm -f build/leno_lsp
+fi
+
+# LSP source files
+LSP_SOURCES=""
+LSP_SOURCES="$LSP_SOURCES lsp_server.c"
+LSP_SOURCES="$LSP_SOURCES lsp_protocol.c"
+LSP_SOURCES="$LSP_SOURCES lsp_document.c"
+LSP_SOURCES="$LSP_SOURCES lsp_diagnostic.c"
+LSP_SOURCES="$LSP_SOURCES lsp_complete.c"
+LSP_SOURCES="$LSP_SOURCES lsp_hover.c"
+LSP_SOURCES="$LSP_SOURCES lsp_definition.c"
+LSP_SOURCES="$LSP_SOURCES json.c"
+LSP_SOURCES="$LSP_SOURCES leno_compiler_lib.c"
+
+# LenoC source files
+LENO_SOURCES=""
+LENO_SOURCES="$LENO_SOURCES ../src/error.c"
+LENO_SOURCES="$LENO_SOURCES ../src/lexer.c"
+LENO_SOURCES="$LENO_SOURCES ../src/ast.c"
+LENO_SOURCES="$LENO_SOURCES ../src/scope.c"
+LENO_SOURCES="$LENO_SOURCES ../src/type.c"
+LENO_SOURCES="$LENO_SOURCES ../src/parser/parser.c"
+LENO_SOURCES="$LENO_SOURCES ../src/parser/parser_utils.c"
+LENO_SOURCES="$LENO_SOURCES ../src/parser/parser_expr.c"
+LENO_SOURCES="$LENO_SOURCES ../src/parser/parser_stmt_control.c"
+LENO_SOURCES="$LENO_SOURCES ../src/parser/parser_stmt_other.c"
+LENO_SOURCES="$LENO_SOURCES ../src/parser/parser_func.c"
+LENO_SOURCES="$LENO_SOURCES ../src/parser/parser_module.c"
+LENO_SOURCES="$LENO_SOURCES ../src/semantic/semantic.c"
+LENO_SOURCES="$LENO_SOURCES ../src/semantic/semantic_upvalue.c"
+LENO_SOURCES="$LENO_SOURCES ../src/semantic/semantic_type.c"
+LENO_SOURCES="$LENO_SOURCES ../src/semantic/semantic_visit.c"
+LENO_SOURCES="$LENO_SOURCES ../src/semantic/semantic_visit_method.c"
+LENO_SOURCES="$LENO_SOURCES ../src/semantic/semantic_visit_ast.c"
+LENO_SOURCES="$LENO_SOURCES ../src/semantic/semantic_visit_func.c"
+LENO_SOURCES="$LENO_SOURCES ../src/semantic/semantic_type_utils.c"
+LENO_SOURCES="$LENO_SOURCES ../src/gc.c"
+LENO_SOURCES="$LENO_SOURCES ../src/value.c"
+LENO_SOURCES="$LENO_SOURCES ../src/string_table.c"
+LENO_SOURCES="$LENO_SOURCES ../src/object/object_string.c"
+LENO_SOURCES="$LENO_SOURCES ../src/object/object_array.c"
+LENO_SOURCES="$LENO_SOURCES ../src/object/object_dict.c"
+LENO_SOURCES="$LENO_SOURCES ../src/object/object_number.c"
+LENO_SOURCES="$LENO_SOURCES ../src/object/object_file.c"
+LENO_SOURCES="$LENO_SOURCES ../src/object/object_draw.c"
+LENO_SOURCES="$LENO_SOURCES ../src/object/object_window.c"
+LENO_SOURCES="$LENO_SOURCES ../src/object/object_event.c"
+LENO_SOURCES="$LENO_SOURCES ../src/object/object_image.c"
+LENO_SOURCES="$LENO_SOURCES ../src/object/object_font.c"
+LENO_SOURCES="$LENO_SOURCES ../src/object/object_struct.c"
+LENO_SOURCES="$LENO_SOURCES ../src/object/object_cstruct.c"
+LENO_SOURCES="$LENO_SOURCES ../src/object/object_face.c"
+LENO_SOURCES="$LENO_SOURCES ../src/object/object_thread.c"
+LENO_SOURCES="$LENO_SOURCES ../src/bound_method.c"
+LENO_SOURCES="$LENO_SOURCES ../src/coroutine.c"
+LENO_SOURCES="$LENO_SOURCES ../src/vm/vm.c"
+LENO_SOURCES="$LENO_SOURCES ../src/codegen/codegen.c"
+LENO_SOURCES="$LENO_SOURCES ../src/codegen/codegen_emit.c"
+LENO_SOURCES="$LENO_SOURCES ../src/codegen/codegen_expr.c"
+LENO_SOURCES="$LENO_SOURCES ../src/codegen/codegen_stmt.c"
+LENO_SOURCES="$LENO_SOURCES ../src/codegen/codegen_func.c"
+LENO_SOURCES="$LENO_SOURCES ../src/codegen/codegen_import.c"
+LENO_SOURCES="$LENO_SOURCES ../src/codegen/codegen_utils.c"
+LENO_SOURCES="$LENO_SOURCES ../src/debug.c"
+LENO_SOURCES="$LENO_SOURCES ../src/native.c"
+LENO_SOURCES="$LENO_SOURCES ../src/bigint.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module_loader.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module_symbol_table.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module_dispatch.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module_compiler.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/io/io.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/types/types.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/times/times.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/arrays/arrays.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/strings/strings.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/maths/maths.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/dicts/dicts.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/structs/structs.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/rands/rands.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/files/files.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/asyncs/asyncs.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/dirs/dirs.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/jsons/jsons.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/sockets/sockets.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/ffi/ffi.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/cstructs/cstructs.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/threads/threads.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/assert/assert.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/sys/sys.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/regexs/regexs.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/guis/guis.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/guis/guis_constants.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/guis/guis_draw.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/guis/guis_window.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/guis/guis_style.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/guis/guis_event.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/guis/guis_image.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/guis/guis_font.c"
+LENO_SOURCES="$LENO_SOURCES ../src/module/guis/leno_guis_log.c"
+LENO_SOURCES="$LENO_SOURCES ../src/platform/platform_thread.c"
+LENO_SOURCES="$LENO_SOURCES ../src/package/package_platform.c"
+LENO_SOURCES="$LENO_SOURCES ../src/package/package_toml.c"
+LENO_SOURCES="$LENO_SOURCES ../src/package/package_init.c"
+LENO_SOURCES="$LENO_SOURCES ../src/package/package_resolve.c"
+LENO_SOURCES="$LENO_SOURCES ../src/package/package_install.c"
+
+# Platform-specific sources
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    LENO_SOURCES="$LENO_SOURCES ../src/module/guis/leno_guis_macos.c"
+elif [[ "$OSTYPE" == "linux"* ]]; then
+    LENO_SOURCES="$LENO_SOURCES ../src/module/guis/leno_guis_linux.c"
+fi
+
+# Platform-specific libraries
+LIBS="-lm -lpthread -ldl"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    LIBS="$LIBS -framework AppKit"
+else
+    LIBS="$LIBS -lX11"
+fi
+
+gcc -o build/leno_lsp $LSP_SOURCES $LENO_SOURCES -I../src -Wall -Wextra -std=c99 -O2 $LIBS
+
+echo "Build successful: build/leno_lsp"
